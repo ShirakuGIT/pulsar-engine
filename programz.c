82b4bd3232085f3091c7ee584e7ad6e0743f5826
Shivaram Kumar <shiraku@Shivarams-MacBook-Pro.local>
2022-03-02 16:39:17 +0530

    Wrote an integer reading program in C

---
+++ programz.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdint.h>
+
+int main() {
+
+	int user_int;
+
+	scanf("%20d", &user_int);
+
+	printf("%d", user_int);
+
+	printf("%s", "\n");
+
+	return 0;
+
+}
